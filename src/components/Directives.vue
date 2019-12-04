e0f932e1f9686c08f47ab21721dae8521726cd4f
Roberto Morales <roberto@Robertos-MacBook-Pro.local>
2019-12-03 22:31:14 -0600

    complete directives component

--- src/components/Directives.vue
+++ src/components/Directives.vue
@@ -1,7 +1,36 @@
 <template>
-  <div>Directives</div>
+  <div class="directives">
+    <h1>Built-in Directives</h1>
+    <p v-text="'Adding text using v-text directive'"></p>
+    <p v-html="'<strong>Adding html using v-html</strong>'"></p>
+    <hr />
+    <h1>Custom Directives</h1>
+    <p v-highlight:background.delayed="'cyan'">Using a global custom directive</p>
+    <p v-underline.blink="{ color1:'aliceblue', color2: 'salmon', delayed: 1000 }"
+    >Using a local custom directive</p>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  directives: {
+    underline: {
+      bind(el, binding) {
+        if (binding.modifiers.blink) {
+          const mainColor = binding.value.color1;
+          const secondColor = binding.value.color2;
+          let currentColor = mainColor;
+          setInterval(() => {
+            if (currentColor === mainColor) {
+              currentColor = secondColor;
+            } else {
+              currentColor = mainColor;
+            }
+            el.style.backgroundColor = currentColor; // eslint-disable-line
+          }, binding.value.delayed);
+        }
+      },
+    },
+  },
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -10,6 +10,25 @@ Vue.config.productionTip = false;
 /* Global Components */
 Vue.component('app-code-snippet', CodeSnippet);
 
+/* Global Directive */
+Vue.directive('highlight', {
+  bind(el, binding) {
+    let delay = 0;
+    // el.style.backgroundColor = 'cyan'
+    // el.style.backgroundColor = binding.value
+    if (binding.modifiers.delayed) {
+      delay = 4000;
+    }
+    setTimeout(() => {
+      if (binding.arg === 'background') {
+        el.style.backgroundColor = binding.value; // eslint-disable-line
+      } else {
+        el.style.color = binding.value; // eslint-disable-line
+      }
+    }, delay);
+  },
+});
+
 new Vue({
   /* Routing */
   router,
