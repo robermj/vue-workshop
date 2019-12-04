85ae59fc29c0a2ee5da46af30f2611523d1c9cf7
Roberto Morales <roberto@Robertos-MacBook-Pro.local>
2019-12-03 22:10:34 -0600

    complete conditionals, lists and forms components

--- src/components/Conditionals&Lists.vue
+++ src/components/Conditionals&Lists.vue
@@ -1,7 +1,68 @@
 <template>
-  <div>Conditionals & Lists</div>
+  <div class="conditionalsAndLists">
+    <h1>Conditionals & Lists</h1>
+    <h3>Conditionals</h3>
+    <div class="alert alert-primary">
+      <p><b>v-if / v-else</b> removes the element from the dom</p>
+    </div>
+    <button class="btn btn-dark" @click="displayIf = !displayIf">Show / Hide</button>
+    <br><br>
+    <div class="alert alert-success" v-if="displayIf">This is a success message!</div>
+    <div class="alert alert-danger" v-else>This is an error message...</div>
+    <div class="alert alert-primary">
+      <p><b>v-show</b> hides the element in the dom, but doesn't remove it</p>
+    </div>
+    <button class="btn btn-dark" @click="displayShow = !displayShow">Show / Hide</button>
+    <br><br>
+    <div class="alert alert-warning" v-show="displayShow">This is a warning message!</div>
+    <button class="btn btn-dark" @click="displayTemplate = !displayTemplate">Show / Hide</button>
+    <br><br>
+    <template v-if="displayTemplate">
+      <div class="alert alert-secondary"><p>Inside A Template</p></div>
+      <div class="alert alert-secondary"><p>Another way of using conditionals</p></div>
+      <div class="alert alert-secondary"><p>To show / hide multiple elements</p></div>
+    </template>
+    <hr>
+    <h3>Lists</h3>
+    <div class="alert alert-primary">
+      <p><b>v-for</b> for rendering multipe items</p>
+    </div>
+    <p>Simple Array</p>
+    <ul class="list-group">
+      <li class="list-group-item" v-for="(item, i) in simpleArray" :key="i">{{ item }}</li>
+    </ul>
+    <p>Objects Array</p>
+    <ul class="list-group">
+      <li class="list-group-item" v-for="({id, name, attr}, i) in objectArray" :key="id">
+        <p>ID: {{ id }}</p>
+        <p>Name: {{ name }}</p>
+        <p>Attr: {{ attr }}</p>
+        <p>Array Index: {{ i }}</p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      displayIf: true,
+      displayShow: true,
+      displayTemplate: true,
+      simpleArray: ['Club Sport Herediano', 'Liverpool', 'Napoli', 'Borussia Dortmund'],
+      objectArray: [
+        { id: 1, name: 'Foo', attr: '25' },
+        { id: 2, name: 'Goo', attr: '13' },
+        { id: 3, name: 'Hoo', attr: '66' },
+      ],
+    };
+  },
+};
 </script>
+
+<style scoped>
+.conditionalsAndLists {
+  margin-bottom: 5rem;
+}
+</style>
--- src/components/Forms.vue
+++ src/components/Forms.vue
@@ -1,7 +1,124 @@
 <template>
-  <div>Forms</div>
+  <div class="forms">
+    <h1>Forms</h1>
+    <div class="row">
+      <div class="col-sm-6">
+        <form>
+          <!-- INPUTS AND TEXTAREA -->
+          <div class="form-group">
+            <label for="email">Mail</label>
+            <input type="text" id="email" class="form-control" v-model="user.email" />
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              class="form-control"
+              v-model.lazy="user.password" />
+          </div>
+          <div class="form-group">
+            <label for="age">Age</label>
+            <input type="number" id="age" class="form-control" v-model.number="user.age" />
+          </div>
+          <div class="form-group">
+            <label for="message">Message</label>
+            <br />
+            <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
+            <textarea id="message" rows="5" class="form-control" v-model="message"></textarea>
+          </div>
+          <!-- CHECKBOX -->
+          <div class="form-group">
+            <label for="sendmail">
+              <input
+              type="checkbox"
+              id="sendmail"
+              value="SendMail"
+              v-model="mailOptions" /> Send Mail
+            </label>
+            <label for="sendInfomail">
+              <input type="checkbox" id="sendInfomail" value="SendInfoMail" v-model="mailOptions" />
+              Send Infomail
+            </label>
+          </div>
+          <!-- RADIO BUTTONS -->
+          <div class="form-group">
+            <label for="male">
+              <input type="radio" id="male" value="Male" v-model="user.gender" /> Male
+            </label>
+            <label for="female">
+              <input type="radio" id="female" value="Female" v-model="user.gender" /> Female
+            </label>
+            <label for="other">
+              <input type="radio" id="other" value="Other" v-model="user.gender" /> Other
+            </label>
+            <!-- SELECT -->
+            <div class="from-group">
+              <label for="priority">Priority</label>
+              <select id="priority" class="form-control" v-model="priority">
+                <option v-for="(option, i) in priorities" :key="i">{{ option }}</option>
+              </select>
+            </div>
+            <br />
+            <br />
+            <div class="form-group">
+              <button class="btn btn-primary" @click.prevent.stop="sendData">Submit!</button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div class="col-sm-4">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4>Your Data</h4>
+          </div>
+          <div class="panel-body">
+            <p>Mail: {{ user.email }}</p>
+            <p>Password: {{ user.password }}</p>
+            <p>Age: {{ user.age }}</p>
+            <p style="white-space: pre">Message: {{ message }}</p>
+            <p>
+              <strong>Send Mail?</strong>
+            </p>
+            <ul>
+              <li v-for="(option, i) in mailOptions" :key="i">{{ option }}</li>
+            </ul>
+            <p>Gender: {{ user.gender }}</p>
+            <p>Priority: {{ priority }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      user: {
+        email: '',
+        password: '',
+        age: 0,
+        gender: '',
+      },
+      message: 'Change this text...',
+      mailOptions: [],
+      priorities: ['Normal', 'Urgent', 'Critical'],
+      priority: 'Normal',
+      isFormSubmitted: false,
+    };
+  },
+  methods: {
+    sendData() {
+      this.isFormSubmitted = true;
+    },
+  },
+};
 </script>
+
+<style scoped>
+.forms {
+  margin-bottom: 5rem;
+}
+</style>
